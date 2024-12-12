00b3c86f97b0f8836703763c1f3088770dff34c8
meshal <imeshalfm@gmail.com>
2024-12-12 18:46:59 +0300

    init project

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { TamaguiProvider, XStack, YStack, Text, Theme, View } from 'tamagui';
+import tamaguiConfig from './tamagui.config';
+
+export default function App() {
+  return (
+    <TamaguiProvider config={tamaguiConfig}>
+      <Theme name="light">
+        <Theme name="purple">
+          <StatusBar style="auto" />
+          <View f={1}>
+            <YStack f={1} bg="$background" ai="center" jc="center">
+              <XStack space="$4" ai="center">
+                <Text fontSize="$6" fontWeight="bold">
+                  Welcome to Tamagui + Expo
+                </Text>
+              </XStack>
+            </YStack>
+          </View>
+        </Theme>
+      </Theme>
+    </TamaguiProvider>
+  );
+}
